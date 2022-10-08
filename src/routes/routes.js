c4f19f36d266814fdc533dd211140a1b40c205b1
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-10-09 01:02:40 +0600

    books loader

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from '../components/Home';
 import Books from '../components/Books';
+import BookDetails from '../components/BookDetails';
 
 export const router = createBrowserRouter([
     {
@@ -18,7 +19,13 @@ export const router = createBrowserRouter([
             },
             {
                 path:'/books',
+                loader: () => fetch('https://api.itbook.store/1.0/new'),
                 element: <Books></Books>
+            },
+            {
+                path:'/book/:bookId',
+                loader: ({params}) => fetch(`https://api.itbook.store/1.0/books/${params.bookId}`),
+                element: <BookDetails></BookDetails>
             }
         ]
     }
